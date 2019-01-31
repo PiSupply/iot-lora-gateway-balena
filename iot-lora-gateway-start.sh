b6ed7f1856867629b7a6d677a7c677e959e10dbe
Ryan Walmsley <ryanteck@gmail.com>
2019-01-31 15:13:39 +0000

    Add command

---
+++ iot-lora-gateway-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d "/sys/class/gpio/gpio22" ]; then
+    echo "22" > /sys/class/gpio/export
+fi
+echo "out" > /sys/class/gpio/gpio22/direction
+echo "1" > /sys/class/gpio/gpio22/value
+sleep 1
+echo "0" > /sys/class/gpio/gpio22/value
