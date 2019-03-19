11ed8e022520bffbe1ca958fbb4dc072a8734931
Ryan Walmsley <ryanteck@gmail.com>
2019-03-19 11:22:21 +0000

    Continuing Work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # iot-lora-gateway-balena
+
+This Github Repo is a setup for using the Pi Supply IoT LoRa Gateway with Balena Cloud and is based on the guide at https://github.com/jpmeijers/ttn-resin-gateway-rpi with some optimisations and tweaks mad efor the Pi Supply Gateway.
--- configureGateway.py
+++ configureGateway.py
@@ -5,3 +5,18 @@ import json
 import time
 
 print("Gateway Configuring")
+
+#Lets configure this how we do in our PHP version while also including balena variables
+
+
+
+#Set the Gateway ID to part of the mac address
+macAddress =
+gatewayId = "504953" +
+
+#Get Server ID
+os.environ.get("TTN_ID")
+#Get TTN Key
+os.environ.get("TTN_KEY")
+
+#That's all we need from balena
