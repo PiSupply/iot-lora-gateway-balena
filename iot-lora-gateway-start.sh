f2b2ebd1febf80c0232e4cbd564a3f6084ce5927
Ryan Walmsley <ryanteck@gmail.com>
2019-03-19 12:07:47 +0000

    Change bits around

--- iot-lora-gateway-start.sh
+++ iot-lora-gateway-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python /opt/iotloragateway/configureGateway.py
 if [ ! -d "/sys/class/gpio/gpio22" ]; then
     echo "22" > /sys/class/gpio/export
 fi
@@ -6,3 +7,4 @@ echo "out" > /sys/class/gpio/gpio22/direction
 echo "1" > /sys/class/gpio/gpio22/value
 sleep 1
 echo "0" > /sys/class/gpio/gpio22/value
+/opt/iotloragatway/iot-lora-gateway
